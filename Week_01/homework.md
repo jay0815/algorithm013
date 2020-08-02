59e19b28be27f72ce9a041c12f550880c2ec7cf7
jack <543612779@qq.com>
2020-08-02 22:06:44 +0800

    [jackQ] - add - week - 1 - summary

--- Week_01/homework.md
+++ Week_01/homework.md
@@ -1,8 +1,6 @@
 ### 简单：
 * 用 add first 或 add last 这套新的 API 改写 Deque 的代码
-  - [](./)
 * 分析 Queue 和 Priority Queue 的源码
-  - [](./)
 * 删除排序数组中的重复项（Facebook、字节跳动、微软在半年内面试中考过）
   - [removeDuplicates](./removeDuplicates.ts)
 * 旋转数组（微软、亚马逊、PayPal 在半年内面试中考过）
@@ -10,7 +8,7 @@
 * 合并两个有序链表（亚马逊、字节跳动在半年内面试常考）
   - [](./)
 * 合并两个有序数组（Facebook 在半年内面试常考）
-  - [](./)
+  - [merge](./merge.ts)
 * 两数之和（亚马逊、字节跳动、谷歌、Facebook、苹果、微软在半年内面试中高频常考）
   - [twoSum](./twoSum.ts)
 * 移动零（Facebook、亚马逊、苹果在半年内面试中考过）
@@ -21,6 +19,8 @@
   - [maxArea](./maxArea.ts)
 * 加一（谷歌、字节跳动、Facebook 在半年内面试中考过）
   - [plusOne](./plusOne.ts)
+* 有效的括号
+  - [validParentheses](./validParentheses.ts)
 ### 中等：
 * 设计循环双端队列（Facebook 在 1 年内面试中考过）
   - [](./)
---
+++ Week_01/merge.ts
@@ -0,0 +1,36 @@
+export default {
+  /**
+   * 思路：
+   * 要求将 num2 合并到  num1上
+   * 所以 num1 最后 大小为 n+m
+   * 
+   * 从前向后排涉及到 数组的移动，会产生 [1, m]的时间消耗
+   * 从后往前则是 开辟新的内存地址 或者 数据交换，消耗时间基本可以忽律不记
+   * 所以选择 从后往前排
+   * 
+   * 因为 num2 是主动 合并的一方
+   * 所以 当num2 中的 数据全部比较完之后，
+   * 即可视为 合并完成
+   * 终止条件 为 n-- -> n === 0;
+   * 
+   * 考虑到 num1 可能为 空数组 或者 在n 变成0前，m-- -> n < 0
+   * 此时 num1[m] 会为 undefined
+   * 任何 数值 与 undefined 的任何比较 都是 false
+   * 所以 当  num1[m] 为 undefined时, 即视为 当前 应该排序 num2[m]
+   */
+  flashBack: function (num1: number[], m: number, num2: number[], n: number) {
+    let index1 = m - 1;
+    let index2 = n - 1;
+    let index3 = m + n - 3;
+    while(index2 >= 0) {
+      if(typeof num1[index1] === 'undefined' || num1[index1] < num2[index2]){
+        num1[index3] = num2[index2]
+        index2--;
+      }else {
+        num1[index1] = num1[index1]
+        index1--;
+      }
+      index3--;
+    }
+  }
+}
